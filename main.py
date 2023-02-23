599d40f715a2a8742f2bee3ba8a4ec8c8727c72e
ekpramod <ekpramod2@gmail.com>
2023-02-23 07:52:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import shutil
 import streamlit as st
 
 cwd = os.getcwd()
-dir = os.listdir(cwd)
-print cwd
-print dir
+dir_list = os.listdir(cwd)
+print(cwd)
+print(dir_list)
 
 st.write("Hello")
 
