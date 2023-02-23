4db802ada59b9881b5ffa6334359dfd08f8290cb
ekpramod <ekpramod2@gmail.com>
2023-02-23 07:33:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import torch
 import io
 import os
 import shutil
-from PIL import Image
-import cv2
-import streamlit as st
-import numpy as np
-from detect_track import run
+
 
 print(os.listdir())
