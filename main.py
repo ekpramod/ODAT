3b6d1b1fc10eff890f70397610c0d0bd83a9dfd1
ekpramod <ekpramod2@gmail.com>
2023-02-23 08:11:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import streamlit as st
 
 cwd = os.getcwd()
 dir_list = os.listdir(cwd)
-print(cwd)
-print(dir_list)
 
 st.write(cwd)
 st.write(dir_list)
@@ -21,6 +19,6 @@ for i in range(10):
 f.close()     
 
 dir_list = os.listdir()
-print(dir_list)
+st.write(dir_list)
 
 os.chdir(cwd)
